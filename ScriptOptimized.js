12b74c8209b4719447ae11f331f1d322f3d9f2e4
whoissubedibibek <subedibibek00@outlook.com>
2022-01-14 05:34:30 +0545

    code optimized for linkedIn

---
+++ ScriptOptimized.js
@@ -0,0 +1,9 @@
+var toBeClicked = document.querySelectorAll("footer.mt2 button");
+lengthtoBeClicked = toBeClicked .length;
+
+for( i=0; i< lengthtoBeClicked; i++ ){
+    if(document.querySelectorAll("button")[i].innerText==("Connect")|| ("connect")){
+        toBeClicked[i].click();
+        console.log("button clicked");}
+    else console.log("this is not a button you want to press");
+}
