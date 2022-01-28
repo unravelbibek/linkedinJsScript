346b3951450bd759daa45a67aa8baae1fd1018b8
whoissubedibibek <subedibibek00@outlook.com>
2022-01-28 12:32:31 +0545

    code refactored for more suggestion for you section

--- ScriptOptimized.js
+++
@@ -1,9 +0,0 @@
-var toBeClicked = document.querySelectorAll("footer.mt2 button");
-lengthtoBeClicked = toBeClicked .length;
-
-for( i=0; i< lengthtoBeClicked; i++ ){
-    if(document.querySelectorAll("button")[i].innerText==("Connect")|| ("connect")){
-        toBeClicked[i].click();
-        console.log("button clicked");}
-    else console.log("this is not a button you want to press");
-}
--- moreOptimized.js
+++ moreOptimized.js
@@ -1,9 +1,12 @@
-var toBeClicked = document.querySelectorAll("footer.mt2 button");
+// more suggestion for you section
+
+var toBeClicked = document.querySelectorAll(".relative.pb2 .discover-fluid-entity-list.discover-fluid-entity-list--default-width-cards .ember-view.display-flex .discover-entity-type-card__bottom-container .mt2 button span.artdeco-button__text");
 lengthtoBeClicked = toBeClicked .length;
 
 for( i=0; i< lengthtoBeClicked; i++ ){
-    if(document.querySelectorAll("button")[i].innerText==("Connect")){
-        toBeClicked[i].click();
+    if(toBeClicked[i].innerText==("Connect")){
+        toBeClicked[i].parentElement.click();
         console.log("button clicked");}
     else console.log("this is not a button you want to press");
 }
+
